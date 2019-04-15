038577ec497a0787a1bb4ab8569d0449f67d934c
kkaatt <kjone203.e@gmail.com>
2019-04-15 14:07:12 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Serve Static Assets
+app.use(express.static(__dirname + '/public'));
+
+// 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+// Start Server
+app.listen(PORT, () => console.log(`Server  running on port ${PORT}`));
